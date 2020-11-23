af3a98e0e81df9eb2569709dc2517a3b6a5d9434
Shahir Ali <shahirsheikali@gmail.com>
2020-11-22 18:01:16 -0800

    push

--- dogs.py
+++ dogs.py
@@ -4,8 +4,8 @@ class Dog:
         self.name = name
         print("dog initialized!")
 
-# the same instantiation call that creates a Dog object,
-# but now we've assigned it to the value of the my_dog variable
 my_dog = Dog("Rex")
-print(my_dog)
-print(my_dog.name)
+# Adding the "breed" property on the fly
+my_dog.breed = "SuperDog"
+# will print "SuperDog"
+print(my_dog.breed)
